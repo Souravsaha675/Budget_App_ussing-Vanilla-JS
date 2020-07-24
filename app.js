ed2a6eba77799eee4f4535f87f8df7edf5bb8708
sourav saha <souravsaha675@gmail.com>
2020-07-25 03:15:04 +0530

    Readding input data

---
+++ app.js
@@ -0,0 +1,81 @@
+var budgetController = (function(){
+    
+   
+
+})();
+
+
+
+
+
+
+var UIController = (function(){
+
+    var DomString = {
+        inputType : ".add__type",
+        inputDescription : ".add__description",
+        inputValue : ".add__value",
+        inputButton: ".add__btn"
+    }
+
+    return {
+        getInput:function(){
+            return{
+                type:document.querySelector(DomString.inputType).value,
+                description:document.querySelector(DomString.inputDescription).value,
+                value:document.querySelector(DomString.inputValue).value
+            }
+        },
+
+        getDomString:function(){
+            return DomString;
+        }
+    }    
+
+})();
+
+
+
+
+
+
+var Controller = (function(budgetControl,UIControl){
+
+    var setupEventListeners = function(){
+
+        var Dom = UIControl.getDomString();
+
+        document.querySelector(Dom.inputButton).addEventListener("click", control);
+
+        document.addEventListener("keypress", event => {
+            if (event.keyCode === 13 || event.which === 13) {
+                control();
+            }
+        })
+
+    }
+
+    var control = function () {
+
+        var input = UIControl.getInput();
+
+        console.log(input);
+
+        //console.log("It's works");
+    }
+
+
+    return {
+        init : function(){
+            
+            console.log("Application has started.");
+            
+            setupEventListeners();
+        }
+    }
+    
+    
+})(budgetController,UIController)
+
+
+Controller.init();
